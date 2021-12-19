ed8aedbc33a96632406263e07dd9564b8eb866a8
avonamardba <avonamardba@gmail.com>
2021-12-19 22:22:01 +0300

    Scripted auto-commit on change (2021-12-19 22:22:01) by gitwatch.sh

--- src/main/java/bigdata/labs/lab5/StorageActor.java
+++ src/main/java/bigdata/labs/lab5/StorageActor.java
@@ -1,44 +1,22 @@
 package bigdata.labs.lab5;
 
 import akka.actor.AbstractActor;
+import akka.actor.Props;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.TreeMap;
 
 public class StorageActor extends AbstractActor {
-    private final HashMap<String, ArrayList<TestMessage>> storage = new HashMap<>();
+    private final HashMap<TestURL, Long> storage = new HashMap<>();
 
-    private ArrayList<TestMessage> getTests(String packageId) throws Exception {
-        if (this.storage.containsKey(packageId)) {
-            return this.storage.get(packageId);
-        } else throw new Exception("No such package");
+    public StorageActor() {
+        this.storage = new TreeMap<m>();
     }
 
-    private RequestAnswers makeResults(String packageId) throws Exception {
-        ArrayList<TestResult> answers = new ArrayList<>();
-        if (this.storage.containsKey(packageId)) {
-            for (TestMessage testMessage : this.getTests(packageId)) {
-                String expectedResult = testMessage.getExpectedResult();
-                String actualResult = testMessage.getActualResult();
-                TestResult testResult = new TestResult(expectedResult,
-                        actualResult,
-                        actualResult.equals(expectedResult));
-                answers.add(testResult);
-            }
-            return new RequestAnswers(packageId, answers);
-        } else throw new Exception("No such package");
-    }
-
-    private void putTest(TestMessage testMessage) {
-        String packageId = testMessage.getParent().getPackageId();
-        if (this.storage.containsKey(packageId)) {
-            this.storage.get(packageId).add(testMessage);
-        } else {
-            ArrayList<TestMessage> tests = new ArrayList<>();
-            tests.add(testMessage);
-            this.storage.put(packageId, tests);
-        }
+    public static Props props() {
+        return Props.create(StorageActor.class);
     }
 
     @Override

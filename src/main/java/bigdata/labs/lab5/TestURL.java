e3f2c873b405df66e1ea634bbebe59a9d540dc86
avonamardba <avonamardba@gmail.com>
2021-12-19 22:02:03 +0300

    Scripted auto-commit on change (2021-12-19 22:02:03) by gitwatch.sh

--- src/main/java/bigdata/labs/lab5/TestURL.java
+++ src/main/java/bigdata/labs/lab5/TestURL.java
@@ -1,10 +1,10 @@
 package bigdata.labs.lab5;
 
-public class TestURL implements Comparable<TestURL>{
+public class TestURL implements Comparable<TestURL> {
     private final String url;
-    private final int count;
+    private final Integer count;
 
-    public TestURL(String url, int count) {
+    public TestURL(String url, Integer count) {
         this.url = url;
         this.count = count;
     }
@@ -19,6 +19,9 @@ public class TestURL implements Comparable<TestURL>{
 
     @Override
     public int compareTo(TestURL testURL) {
-        return 0;
+        if (this.url.compareTo(testURL.url) != 0) {
+            return this.url.compareTo(testURL.url);
+        }
+        return this.count.compareTo(testURL.count);
     }
 }
